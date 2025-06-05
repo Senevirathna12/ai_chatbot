dcdc8b5e622f9ce67930787870fb4951937a5557
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2025-06-05 22:39:03 +0530

    Import neccesory components, icons , hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 "use client";
+import { useState, useEffect, useRef } from "react";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+import { Button } from "@/components/ui/button";
+import { Card , CardContent,CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import LandingSections from "@/components/LandingSections";
+
+import { X,MessageCircle, Send, Loader2, ArrowDownCircleIcon  } from "lucide-react";
+import {motion, AnimatePresence} from 'framer-motion';
+import {useChat} from '@ai-sdk/react';
+
 
 export default function Chat() {
   return (
